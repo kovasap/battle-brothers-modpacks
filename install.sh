0e522c0a51b401f2697ff8ad6d2f474950c4313c
Kovas Palunas <kovas.palunas@gmail.com>
2026-01-12 19:41:34 -0800

    big fonts

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ echo $BBROS_INSTALL_DIR
 
 rm "${BBROS_INSTALL_DIR}"*.zip
 cp hardened/* $BBROS_INSTALL_DIR
+rm -r "${BBROS_INSTALL_DIR}"gfx/*
+unzip hardened/BBLargeFontMOD-182-1-0-1572135147.zip -d "${BBROS_INSTALL_DIR}gfx/"
